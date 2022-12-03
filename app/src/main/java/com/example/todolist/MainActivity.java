0cf43ac70928ed07634d9d175cde6ee399206249
anujpophali <anujpophali@gmail.com>
2022-12-03 11:36:26 +0530

    Second Commit

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView l;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> note = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        l = findViewById(R.id.lstView);
     }
+
+    void createMenu(Menu menu){
+        MenuItem i = menu.add(0,0, 0, "Share");
+        MenuItem i1 = menu.add(0,1, 0, "Exit");
+    }
+
+    private boolean menuChoice(MenuItem item){
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        createMenu(menu);
+        createPlus(menu);
+        return true;
+    }
+
+    void createPlus(Menu menu) {
+        MenuInflater inf = getMenuInflater();
+        inf.inflate(R.menu.option_menu,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        return menuChoice(item);
+    }
+
 }
