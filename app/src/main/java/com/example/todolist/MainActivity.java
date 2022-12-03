ee5372578982e8d9ad6b63f20cf86eb35910b891
anujpophali <anujpophali@gmail.com>
2022-12-03 15:32:10 +0530

    Safe Point 2

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.todolist;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -15,6 +17,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,8 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
     ListView listView;
     ArrayAdapter<String> arrayAdapter;
-    ArrayList<String> notes = new ArrayList<>();
-    static HashMap<String,String> titleAndDesc;
+    static ArrayList<String> notes = new ArrayList<>();
+    static HashMap<String,String> titleAndDesc=new HashMap<String,String>();
     static ArrayList<String> titles=new ArrayList<>();
 
 
@@ -51,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         for(String s:notes){
             String[] sarr =s.split("[|||]");
             titles.add(sarr[0]);
-//            titleAndDesc.put(sarr[0],sarr[1]);
+            titleAndDesc.put(sarr[0],sarr[1]);
         }
 
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
@@ -66,7 +69,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                final int itemToDelete = i;
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(R.drawable.ic_baseline_warning_24)
+                        .setTitle("Warning!")
+                        .setMessage("Do You want to delete this note ?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                                        notes.remove(itemToDelete);
+                                        arrayAdapter.notifyDataSetChanged();
+                                        Toast.makeText(getApplicationContext(), "Your Note is successfully Deleted.", Toast.LENGTH_LONG).show();
+
+                                        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.rubancreation.notes", Context.MODE_PRIVATE);
+                                        HashSet<String> set = new HashSet<>(MainActivity.notes);
+                                        sharedPreferences.edit().putStringSet("notes", set).apply();
+
+                                    }
+
+                                }
+                        )
+                        .setNegativeButton("No", null)
+                        .show();
+
+
+                return true;
+
+            }
+
+        });
 
     }
 
